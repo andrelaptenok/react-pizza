581c3d1e692b4175ac3f4789e626c30d019a2253
andrelaptenok <andrelaptenok@gmail.com>
2021-01-08 19:21:24 +0300

    Add pages cart and react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './scss/app.scss';
 
 ReactDOM.render( <
     React.StrictMode >
     <
+    Router >
+    <
     App / >
     <
+    /Router> <
     /React.StrictMode>,
     document.getElementById('root'),
 );
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Cart = () => {
+  return <h1>Корзина</h1>;
+};
