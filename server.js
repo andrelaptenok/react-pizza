a3af9930bc9abe78e901ebbf66ad81a0c8671323
andrelaptenok <andrelaptenok@gmail.com>
2021-02-14 00:10:15 +0300

    edited package

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-    static: './build',
+  static: './build',
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(PORT, () => {
-    console.log('Server is running');
+  console.log('Server is running');
 });
